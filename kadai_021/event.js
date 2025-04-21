21e426f16c822f80c7a39fd57a47bacd0e76c8d5
ROSE211218 <yu22-ri08-coo15@docomo.ne.jp>
2025-04-21 21:40:04 +0900

    課題をアップロード

---
+++ kadai_021/event.js
@@ -0,0 +1,10 @@
+const clickBtn = document.getElementById('btn');
+
+clickBtn.addEventListener('click', () => {
+  const text = document.getElementById('text');
+  setTimeout(() => {
+    text.textContent ='ボタンをクリックしました';
+  }, 2000);
+ 
+});
+
